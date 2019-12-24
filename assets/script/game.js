a78070fbf7dc7d45f55ef80c064e396587956481
phil cowan <philipjcowan1@gmail.com>
2019-12-24 14:08:50 -0700

    declares global vars creates the numberGen function that gives and displays value for the randomNumber and gives a value to each of the crystals

--- assets/script/game.js
+++ assets/script/game.js
@@ -1,5 +1,27 @@
-//the game will generate a random number that will be displayed in the random number div and this will be the users goal score to reach without exceeding it
+//declare some global vars for scores random number and user score and crystal values
+var winCounter=0;
+var lossCounter=0;
+var randomNumber=0;
+var userScore=0;
+var crysOneVal=0;
+var crysTwoVal=0;
+var crysThreeVal=0;
+var crysFourVal=0;
 
+
+
+
+//the game will generate a random number that will be displayed in the random number div and this will be the users goal score to reach without exceeding it
+function numberGen(){
+    randomNumber = (Math.floor(Math.random()*120));
+    crysOneVal = (Math.floor(Math.random()*12));
+    crysTwoVal = (Math.floor(Math.random()*12));
+    crysThreeVal = (Math.floor(Math.random()*12));
+    crysFourVal = (Math.floor(Math.random()*12));
+    //console.log(randomNumber, crysOneVal, crysTwoVal, crysThreeVal, crysFourVal);
+$(".randomnumber").text(randomNumber);
+
+}
 //four different crystal buttons are assigned individual values that will be added to the users total score
 
 //write code for click events on the crystals
