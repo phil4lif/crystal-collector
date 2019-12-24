80a3d06bfc27a8baecba3a73ece5bf988422ade0
phil cowan <philipjcowan1@gmail.com>
2019-12-24 13:40:03 -0700

    pseudocodes gameplay

---
+++ assets/script/game.js
@@ -0,0 +1,16 @@
+//the game will generate a random number that will be displayed in the random number div and this will be the users goal score to reach without exceeding it
+
+//four different crystal buttons are assigned individual values that will be added to the users total score
+
+//write code for click events on the crystals
+
+//each click value is added to the total score
+
+//the total score is displayed in the score div
+
+//if the score matches the random number then the user is victorious
+
+//the scoreboard will update and the game will restart
+
+//if the user score exceeds the random number then the game is lost and will restart
+
